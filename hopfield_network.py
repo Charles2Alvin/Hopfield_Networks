0ce300b87101fb1421bb13f2810756c0ca402652
alvinmo <alvinmo@tencent.com>
2020-09-26 15:32:05 +0800

    feat(main_note): 完成了3.2、3.3的实现 feat(Hopfield_network): 实现了Little model、add_noise方法、energy计算方法

--- hopfield_network.py
+++ hopfield_network.py
@@ -17,11 +17,13 @@ class HopfieldNetwork:
                 print("%.1f  \t" % (self.W[i][j]),  end='')
             print()
 
-    def update(self, p_start):
+    def update_async(self, p_start, mode: str = 'sequential', log: list = None):
         """
         Apply the update rule on the input pattern V to get the restored pattern
         which is an attractor in the network's storage
+        :param mode:
         :param p_start: the start pattern
+        :param log: energy log array to be filled
         :return: the restored pattern
         """
         V = np.copy(p_start)
@@ -29,7 +31,12 @@ class HopfieldNetwork:
         indices = [i for i in range(m)]
         iter = 1
         while True:
+            if log is not None:
+                e = self.energyOf(V)
+                log.append(e)
             cnt = 0
+            if mode == 'random':
+                indices = np.random.permutation(indices)
             for i in indices:
                 value_old = V[i]
                 value_new = np.sign(self.W[:, i].dot(V))
@@ -37,12 +44,55 @@ class HopfieldNetwork:
                     V[i] = value_new
                     cnt += 1
                 # print(i, value_old, value_new, self.W[:, i].dot(V))
+
             if cnt == 0:
                 # print("Converged in %s iterations" % iter)
+                break
+            if iter > 1e3:
                 break
             iter += 1
 
         return V
+
+    def update_sync(self, p_start, log: list = None):
+        """
+        Apply the update rule on the input pattern V to get the restored pattern
+        which is an attractor in the network's storage
+        :param p_start: the start pattern
+        :param log: energy log array to be filled
+        :return: the restored pattern
+        """
+        V = np.copy(p_start)
+        iter = 1
+        while True:
+            if log is not None:
+                e = self.energyOf(V)
+                log.append(e)
+            V_new = np.sign(self.W.dot(V))
+            V = V_new
+            diff = np.sum(np.abs(V - V_new))
+            if diff == 0:
+                break
+            iter += 1
+
+        return V
+
+    def energyOf(self, x):
+        """
+        Compute the energy of the given state
+        :param x: a n-by-1 array pattern which represents a state
+        :return: a float value
+        """
+        # take it as a quadratic form
+        return - x.dot(self.W).dot(x)
+
+    @staticmethod
+    def plot_energy(energy_log):
+        for i in range(len(energy_log)):
+            plt.scatter(i, energy_log[i])
+        plt.plot(energy_log)
+        plt.title("Energy changes")
+        plt.show()
 
 
 f = open("pict.dat")
@@ -58,5 +108,27 @@ p1, p2, p3 = data[0], data[1], data[2]
 X = np.array([p1, p2, p3]).T
 model = HopfieldNetwork()
 model.train(X)
-rp1 = model.update(p1)
-print((rp1 == p1).all())
+
+
+def add_noise(arr: np.ndarray, ratio: float):
+    """
+    Randomly flip a selected number of units
+    :param arr: the array to add noise
+    :param ratio: the ratio of noise
+    :return: the array with noise
+    """
+    p = np.copy(arr)
+    n = len(p)
+    permutation = [i for i in range(n)]
+    m = int(n * ratio)
+    np.random.seed(2)
+    indices = np.random.choice(permutation, m)
+    for index in indices:
+        p[index] = - p[index]
+
+    return p
+
+
+
+
+
